9740749519fcc22a92b9b65b9bdd6d2e377fc7e0
lc1734 <bruce.siwei.luo@gmail.com>
2018-01-04 08:19:28 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='hello',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/lc1734/hello-world',
+      packages=['hello-world'],
+      zip_safe=False)
