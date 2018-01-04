8836f730860c68fa0b317bc52adb8afa57eafe21
lc1734 <bruce.siwei.luo@gmail.com>
2018-01-04 08:38:13 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(name='hello',
       version='0.1',
       description='The funniest joke in the world',
       url='https://github.com/lc1734/hello-world',
-      packages=['hello-world'],
+      packages=['hello'],
       zip_safe=False)
